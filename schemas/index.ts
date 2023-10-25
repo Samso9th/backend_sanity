bd1e76241cbc26eeeb324bab99ae000886bd7823
Samson Agbo <agbosamsonite@gmail.com>
2023-10-25 21:44:59 +0100

    Backend files

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import abouts from './abouts'
+import brands from './brands'
+import testimonials from './testimonials'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import projects from './projects'
+export const schemaTypes = [testimonials, abouts, brands, contact, experiences, skills, projects, workExperience] 
+
