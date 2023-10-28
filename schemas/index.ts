15932619bf72de7f43505206fdecd47e83aee87c
Samson Agbo <agbosamsonite@gmail.com>
2023-10-28 17:34:16 +0100

    testimonial issue fixed

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,6 +5,6 @@ import contact from './contact'
 import experiences from './experiences'
 import skills from './skills'
 import workExperience from './workExperience'
-import projects from './projects'
-export const schemaTypes = [testimonials, abouts, brands, contact, experiences, skills, projects, workExperience] 
+import works from './works'
+export const schemaTypes = [testimonials, abouts, brands, contact, experiences, skills, works, workExperience] 
 
